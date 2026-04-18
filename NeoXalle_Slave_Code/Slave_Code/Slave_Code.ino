5994970559078fde8f0bb11c9f442e198c69847f
Villa <rekirxs@gmail.com>
2026-04-18 16:04:40 -0500

    Update Slave_Code.ino
    Fixed the error which was the ammount of info the adxl was getting, it was not sensible before, now it is.

--- NeoXalle_Slave_Code/Slave_Code/Slave_Code.ino
+++ NeoXalle_Slave_Code/Slave_Code/Slave_Code.ino
@@ -10,6 +10,7 @@
 #define SDA_PIN 7
 #define SCL_PIN 6
 
+#define HIT_THRESHOLD 150
 
 
 
@@ -28,7 +29,8 @@ Crystal frequency:  40MHz
 MAC:                e0:72:a1:d6:44:d8
 */
 
-/* uint8_t masterAddress [] = { 0xe0, 0x72, 0xa1, 0xd6, 0x44, 0xd8};
+
+uint8_t masterAddress [] = { 0xe0, 0x72, 0xa1, 0xd6, 0x44, 0xd8};
 
 typedef struct {
   bool hit;
@@ -36,7 +38,9 @@ typedef struct {
 
 } DataPacket;
 
-DataPacket data;*/
+DataPacket data;
+
+
 
 void writeReg(uint8_t reg, uint8_t val) {
   Wire.beginTransmission(ADXL375_ADDR);
@@ -45,45 +49,25 @@ void writeReg(uint8_t reg, uint8_t val) {
   Wire.endTransmission();
 }
 
-/*void readAccel (float &gx, float &gy, float &gz){
- 
-
-  gx = rx * 0.049f;
-  gy = ry * 0.049f;
-  gz = rz * 0.049f;
-} */
-
 
 void setup() {
-  
   Serial.begin(115200);
   delay(1000);
-
   Wire.begin(SDA_PIN, SCL_PIN);
   delay(100);
-  writeReg(REG_DATA_FORMAT, 0x00);
+
+  writeReg(REG_POWER_CTL, 0x00);
+  writeReg(REG_DATA_FORMAT, 0x0F);
   writeReg(REG_POWER_CTL, 0x08);
 
-    Wire.beginTransmission(ADXL375_ADDR);
-  Wire.write(REG_DATAX_FORMAT);
-  Wire.endTransmission(false);
-  Wire.requestFrom(ADXL375_ADDR,1);
-  uint8_t fmt = Wire.read();
-
-  Serial.print("Data_format register: 0x");
-  Serial.println(fmt, HEX);
-
-
- 
-
- /* WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA);
   esp_now_init(); 
- 
+
   esp_now_peer_info_t peer = {};
   memcpy(peer.peer_addr, masterAddress, 6);
   peer.channel = 0;
   peer.encrypt = false;
-  esp_now_add_peer(&peer);*/
+  esp_now_add_peer(&peer);
 
   Serial.println("Ready");
 }
@@ -93,27 +77,46 @@ void loop() {
   Wire.write(REG_DATAX0);
   Wire.endTransmission(false);
   Wire.requestFrom(ADXL375_ADDR,6);
+
   int16_t rx = (Wire.read() | (Wire.read() << 8));
-   int16_t ry = (Wire.read() | (Wire.read() << 8));
-    int16_t rz = (Wire.read() | (Wire.read() << 8));
+  int16_t ry = (Wire.read() | (Wire.read() << 8));
+  int16_t rz = (Wire.read() | (Wire.read() << 8));
 
- /* float zImpact = abs(gz) - 1.0f;
+  int16_t peak = max({abs(rx), abs(ry), abs(rz)});
 
-  if (gz > HIT_THRESHOLD) {
-    Serial.print("HIT! Z: ");
-    Serial.print(gz, 1);
+  int16_t restZ = 64;
+  int16_t impact = abs(rz) - restZ;
+
+
+
+  if (impact > HIT_THRESHOLD) {
+    float gs = impact * 0.049f;
+
+    Serial.print("HIT! force: ");
+    Serial.print(gs, 1);
     Serial.println("G");
+
+    data.hit = true;
+    data.gs = gs;
+    esp_now_send(masterAddress, (uint8_t *)&data, sizeof(data));
+
     delay(300);
   }
-  data.hit = true;
-  data.gs = gz;
 
-  esp_now_send(masterAddress, (uint8_t *)&data, sizeof(data));
-  delay(300);
- delay(10);*/
 
-   Serial.print("RAW X: "); Serial.print(rx);
+
+
+ delay(10);
+ 
+ static int16_t lastZ = 0;
+ if (abs(rz - lastZ) > 5) {
+    Serial.print("RAW X: "); Serial.print(rx);
     Serial.print("  Y: "); Serial.print(ry);
-     Serial.print("  Z: "); Serial.println(rz);
- delay(300);
+    Serial.print("  Z: "); Serial.println(rz);
+    lastZ = rz;
+ }
+
+
+ 
+ delay(10);
 }
